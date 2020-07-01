482461b216c1257f7403f56b4ce2502297e0219a
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-07-02 02:03:46 +0300

    db.Connect func impl

--- src/rpc-server/db.go
+++ src/rpc-server/db.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	context "context"
+	"errors"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -12,35 +15,56 @@ type LeakEntry struct {
 	ID   primitive.ObjectID `bson:"_id"`
 	Name string             `bson:"name"`
 }
-type DomainEntry struct {
-	ID   primitive.ObjectID `bson:"_id"`
-	Name string             `bson:"name"`
-}
 type EmailEntry struct {
 	ID        primitive.ObjectID `bson:"_id"`
-	DomainID  primitive.ObjectID `bson:"domain_id"`
+	Domain    string             `bson:"domain"`
 	CreatedAt time.Time          `bson:"created_at"`
 	UpdatedAt time.Time          `bson:"updated_at"`
 	Email     string             `bson:"email"`
 }
 
 type LeakEmailRelationEntry struct {
-	EmailID primitive.ObjectID `bson:"email_id"`
-	LeakID  primitive.ObjectID `bson:"eak_id"`
+	EmailID     primitive.ObjectID `bson:"email_id"`
+	LeakID      primitive.ObjectID `bson:"leak_id"`
+	EmailDomain string             `bson:"email_domain"`
 }
 
 type MongoDBConn struct {
 	conn *mongo.Database
 }
 
-func (db *MongoDBConn) Connect(uri string) error {
+func (db *MongoDBConn) Connect(uri string, database string) error {
+	if db == nil {
+		return errors.New("I am not even alive")
+	}
+
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		return err
+	}
+
+	db.conn = client.Database(database)
 	return nil
 }
 
-func (db *MongoDBConn) GetLeaksByEmail() (*Leak, error) {
+func (db *MongoDBConn) GetAllLeaks() ([]*LeakEntry, error) {
 	return nil, nil
 }
 
-func (db *MongoDBConn) GetLeaksByDomainName() (*Leak, error) {
+func (db *MongoDBConn) GetEmailsByLeakID(leakId string) ([]*EmailEntry, error) {
+	return nil, nil
+}
+
+func (db *MongoDBConn) GetLeaksByEmail(email string) ([]*LeakEntry, error) {
+	return nil, nil
+}
+
+func (db *MongoDBConn) GetLeaksByDomain(domain string) ([]*LeakEntry, error) {
+	return nil, nil
+}
+
+func (db *MongoDBConn) GetEmailsByDomainAndLeakID(domain string, leakId string) ([]*EmailEntry, error) {
 	return nil, nil
 }
