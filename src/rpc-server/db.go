f37ac7b86b3095fcf47a6ca90d01acc5b37b8747
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-07-02 01:58:41 +0300

    rpc server stub impl

--- src/rpc-server/db.go
+++ src/rpc-server/db.go
@@ -1,3 +1,46 @@
 package main
 
-type MongoDBConn struct{}
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type LeakEntry struct {
+	ID   primitive.ObjectID `bson:"_id"`
+	Name string             `bson:"name"`
+}
+type DomainEntry struct {
+	ID   primitive.ObjectID `bson:"_id"`
+	Name string             `bson:"name"`
+}
+type EmailEntry struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	DomainID  primitive.ObjectID `bson:"domain_id"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
+	Email     string             `bson:"email"`
+}
+
+type LeakEmailRelationEntry struct {
+	EmailID primitive.ObjectID `bson:"email_id"`
+	LeakID  primitive.ObjectID `bson:"eak_id"`
+}
+
+type MongoDBConn struct {
+	conn *mongo.Database
+}
+
+func (db *MongoDBConn) Connect(uri string) error {
+	return nil
+}
+
+func (db *MongoDBConn) GetLeaksByEmail() (*Leak, error) {
+	return nil, nil
+}
+
+func (db *MongoDBConn) GetLeaksByDomainName() (*Leak, error) {
+	return nil, nil
+}
--- src/rpc-server/server.go
+++ src/rpc-server/server.go
@@ -1,21 +1,167 @@
 package main
 
 import (
+	"errors"
+
 	empty "github.com/golang/protobuf/ptypes/empty"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
 )
 
 type LeakServiceServerHandler struct {
-	db *MongoDBConn
+	DBConnUri string
+	db        *MongoDBConn
+}
+
+func (s *LeakServiceServerHandler) DBConnect() error {
+	if s == nil {
+		return errors.New("Uninitialized server stub")
+	}
+
+	s.db = &MongoDBConn{}
+
+	if err := s.db.Connect(s.DBConnUri); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *LeakServiceServerHandler) ListLeaks(req *empty.Empty, srv LeakService_ListLeaksServer) error {
-	return status.Errorf(codes.Unimplemented, "method ListLeaks not implemented")
+	if s == nil {
+		return status.Errorf(codes.Aborted, "Uninitialized server stub")
+	}
+
+	if s.db == nil {
+		return status.Errorf(codes.Aborted, "Uninitialized database connection")
+	}
+
+	leaks, err := s.db.GetAllLeaks()
+
+	if err != nil {
+		return status.Errorf(codes.Internal, "Unable to get Leak objects")
+	}
+
+	for _, leak := range leaks {
+		leakPkg := &Leak{
+			Id:         leak.ID.Hex(),
+			Name:       leak.Name,
+			EmailCount: 0,
+			Emails:     nil,
+		}
+
+		emailsArr := make([]*Leak_Email, 0)
+		var emailCount int64 = 0
+
+		emails, err := s.db.GetEmailsByLeakID(leakPkg.GetId())
+
+		if err != nil {
+			return status.Errorf(codes.Internal, "Unable to get Email objects")
+		}
+
+		for _, email := range emails {
+			emailCount++
+
+			emailsArr = append(emailsArr, &Leak_Email{
+				Email:            email.Email,
+				Domain:           email.Domain,
+				FirstOccuranceTs: email.CreatedAt.Unix(),
+				LastOccuranceTs:  email.UpdatedAt.Unix(),
+			})
+		}
+
+		leakPkg.Emails = emailsArr
+		leakPkg.EmailCount = emailCount
+
+		if err := srv.Send(leakPkg); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
+
 func (s *LeakServiceServerHandler) GetLeaksByEmail(req *GetLeaksByEmailRequest, srv LeakService_GetLeaksByEmailServer) error {
-	return status.Errorf(codes.Unimplemented, "method GetLeaksByEmail not implemented")
+	if s == nil {
+		return status.Errorf(codes.Aborted, "Uninitialized server stub")
+	}
+
+	if s.db == nil {
+		return status.Errorf(codes.Aborted, "Uninitialized database connection")
+	}
+
+	leaks, err := s.db.GetLeaksByEmail(req.GetEmail())
+
+	if err != nil {
+		return status.Errorf(codes.Internal, "Unable to get Leak objects")
+	}
+
+	for _, leak := range leaks {
+		leakPkg := &Leak{
+			Id:         leak.ID.Hex(),
+			Name:       leak.Name,
+			Emails:     make([]*Leak_Email, 0),
+			EmailCount: 0,
+		}
+
+		if err := srv.Send(leakPkg); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
+
 func (s *LeakServiceServerHandler) GetLeaksByDomain(req *GetLeaksByDomainRequest, srv LeakService_GetLeaksByDomainServer) error {
-	return status.Errorf(codes.Unimplemented, "method GetLeaksByDomain not implemented")
+	if s == nil {
+		return status.Errorf(codes.Aborted, "Uninitialized server stub")
+	}
+
+	if s.db == nil {
+		return status.Errorf(codes.Aborted, "Uninitialized database connection")
+	}
+
+	leaks, err := s.db.GetLeaksByDomain(req.GetDomain())
+
+	if err != nil {
+		return status.Errorf(codes.Internal, "Unable to get Leak objects")
+	}
+
+	for _, leak := range leaks {
+		leakPkg := &Leak{
+			Id:         leak.ID.Hex(),
+			Name:       leak.Name,
+			Emails:     nil,
+			EmailCount: 0,
+		}
+
+		emailsArr := make([]*Leak_Email, 0)
+		var emailCount int64 = 0
+
+		emails, err := s.db.GetEmailsByDomainAndLeakID(req.GetDomain(), leakPkg.GetId())
+
+		if err != nil {
+			return status.Errorf(codes.Internal, "Unable to get Email objects")
+		}
+
+		for _, email := range emails {
+			emailCount++
+
+			emailsArr = append(emailsArr, &Leak_Email{
+				Email:            email.Email,
+				Domain:           email.Domain,
+				FirstOccuranceTs: email.CreatedAt.Unix(),
+				LastOccuranceTs:  email.UpdatedAt.Unix(),
+			})
+		}
+
+		leakPkg.Emails = emailsArr
+		leakPkg.EmailCount = emailCount
+
+		if err := srv.Send(leakPkg); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
