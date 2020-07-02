01a7a55393756afa2d5ab15a790d6fac472f7d81
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-07-02 22:38:03 +0300

    corrected gateway code generation point

--- src/compile.sh
+++ src/compile.sh
@@ -8,4 +8,4 @@ rm -f rpc-server/server_stub
 
 # Compile proto files
 protoc -Iproto/ -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I/usr/local/include --go_out=plugins=grpc:rpc-server proto/*.proto
-protoc -Iproto/ -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I/usr/local/include --grpc-gateway_out=logtostderr=true:rest-server proto/*.proto
+protoc -Iproto/ -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I/usr/local/include --go_out=plugins=grpc:rest-server --grpc-gateway_out=logtostderr=true:rest-server proto/*.proto
