2fed1c7547a3b5878fcddcee4a3ebedb3d8a1f2f
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-07-02 13:42:40 +0300

    seperated rest server code from rpc server

--- src/compile.sh
+++ src/compile.sh
@@ -3,8 +3,9 @@ set -e
 
 # Make the cleaning
 rm -f rpc-server/*.pb.go
-rm -f rpc-server/*.pb.gw.go
+rm -f rest-server/*.pb.gw.go
 rm -f rpc-server/server_stub
 
 # Compile proto files
-protoc -Iproto/ -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I/usr/local/include --go_out=plugins=grpc:rpc-server --grpc-gateway_out=logtostderr=true:rpc-server proto/*.proto
+protoc -Iproto/ -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I/usr/local/include --go_out=plugins=grpc:rpc-server proto/*.proto
+protoc -Iproto/ -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I/usr/local/include --grpc-gateway_out=logtostderr=true:rest-server proto/*.proto
